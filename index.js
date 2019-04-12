568e8022298f267652f7db97d870028a4cbb5ffa
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 03:41:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var recipe = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  recipe[key] = value;
-  return recipe
+  
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  recipe[key] = value;
+  return recipe
 }
 
 function deleteFromObjectByKey(object, key){
